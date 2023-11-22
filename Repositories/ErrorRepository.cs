d8ecf052184d3909fac8be64e5d6f7fc6123e150
Krzysztof <taajny@gmail.com>
2023-11-22 22:44:57 +0100

    zaimplementowana logika biznesowa

---
+++ Repositories/ErrorRepository.cs
@@ -0,0 +1,24 @@
+﻿using ReportService.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Repositories
+{
+    public class ErrorRepository
+    {
+        public List<Error> GetErrors(int intervalInMinutes)
+        {
+            // pobieranie z bazy danych
+
+            return new List<Error>
+            {
+                new Error { Message="Błąd testowy 1", Date = DateTime.Now },
+                new Error { Message="Błąd testowy 1", Date = DateTime.Now }
+
+            };
+        }   
+    }
+}
